f0ba4ff4fcf69b3c13613db7f7b49628696d7e80
tsula21 <ika.tsulaia21@mail.ru>
2022-07-02 18:38:43 +0400

    'fff'

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 // VARIABLES
 // MAIN MENU
-const home = document.querySelector('#home');
-const about = document.querySelector('#about');
-const portfolio = document.querySelector('#portfolio');
-const contact = document.querySelector('#contact');
+const home = document.querySelector("#home");
+const about = document.querySelector("#about");
+const portfolio = document.querySelector("#portfolio");
+const contact = document.querySelector("#contact");
 // resp menu
-const menuBar = document.querySelector('.barMenuBtn');
-const closeMenu = document.querySelector('.closebtn');
-const allSideBar = document.querySelector('.responsive__menu');
+const menuBar = document.querySelector(".barMenuBtn");
+const closeMenu = document.querySelector(".closebtn");
+const allSideBar = document.querySelector(".responsive__menu");
 console.log(closeMenu);
 
 // ADRESS(URL)
@@ -17,17 +17,17 @@ const page3 = "https://tsula21.github.io/page-3/";
 const page4 = "https://tsula21.github.io/page-4/";
 
 //html
-let progressBarHtml = document.querySelector('.html');
-let valueContainerHtml = document.querySelector('.valhtml');
+let progressBarHtml = document.querySelector(".html");
+let valueContainerHtml = document.querySelector(".valhtml");
 // CSS
-let progressBarCss = document.querySelector('.css');
-let valueContainerCss = document.querySelector('.valcss');
+let progressBarCss = document.querySelector(".css");
+let valueContainerCss = document.querySelector(".valcss");
 // JAVASCRIPT
-let progressBarJs = document.querySelector('.js');
-let valueContainerJs = document.querySelector('.valjs');
+let progressBarJs = document.querySelector(".js");
+let valueContainerJs = document.querySelector(".valjs");
 // PHOTOSHOP
-let progressBarPs = document.querySelector('.ps');
-let valueContainerPs = document.querySelector('.valps');
+let progressBarPs = document.querySelector(".ps");
+let valueContainerPs = document.querySelector(".valps");
 
 let progressValue = 0;
 let progressValueJs = 0;
@@ -35,8 +35,8 @@ let progressValuePs = 0;
 // END VALUES
 let progressEndValueHtml = 81;
 let progressEndValueCss = 80;
-let progressEndValueJs = 40;
-let progressEndValuePs = 10;
+let progressEndValueJs = 60;
+let progressEndValuePs = 40;
 
 let speed = 20;
 
@@ -45,88 +45,85 @@ console.log(valueContainerPs);
 
 // Event Listeners
 // MAIN MENU
-home.addEventListener('click', () => {
-    window.open(page1, '_top');
+home.addEventListener("click", () => {
+  window.open(page1, "_top");
 });
 
-about.addEventListener('click', () => {
-    window.open(page2, '_top');
+about.addEventListener("click", () => {
+  window.open(page2, "_top");
 });
 
-portfolio.addEventListener('click', () => {
-    window.open(page3, '_top');
+portfolio.addEventListener("click", () => {
+  window.open(page3, "_top");
 });
 
-contact.addEventListener('click', () => {
-    window.open(page4, '_top');
+contact.addEventListener("click", () => {
+  window.open(page4, "_top");
 });
 
-menuBar.addEventListener('click',openMenu);
-closeMenu.addEventListener('click',menuCloes);
+menuBar.addEventListener("click", openMenu);
+closeMenu.addEventListener("click", menuCloes);
 
 // HTML
 let progressHtml = setInterval(() => {
-    progressValue++;
-    valueContainerHtml.textContent = `${progressValue}%`;
-    progressBarHtml.style.background = `conic-gradient(
+  progressValue++;
+  valueContainerHtml.textContent = `${progressValue}%`;
+  progressBarHtml.style.background = `conic-gradient(
         #ffb400 ${progressValue * 3.6}deg,
         #252525 ${progressValue * 3.6}deg
     )`;
-    if(progressValue == progressEndValueHtml){
-        clearInterval(progressHtml);
-    }
+  if (progressValue == progressEndValueHtml) {
+    clearInterval(progressHtml);
+  }
 }, speed);
 
 // CSS
 let progressCss = setInterval(() => {
-    progressValue++;
-    valueContainerCss.textContent = `${progressValue}%`;
-    progressBarCss.style.background = `conic-gradient(
+  progressValue++;
+  valueContainerCss.textContent = `${progressValue}%`;
+  progressBarCss.style.background = `conic-gradient(
         #ffb400 ${progressValue * 3.6}deg,
         #252525 ${progressValue * 3.6}deg
     )`;
-    if(progressValue == progressEndValueCss){
-        clearInterval(progressCss);
-    }
+  if (progressValue == progressEndValueCss) {
+    clearInterval(progressCss);
+  }
 }, speed);
 
 // JAVASCRIPT
 let progressJs = setInterval(() => {
-    progressValueJs++;
-    valueContainerJs.textContent = `${progressValueJs}%`;
-    progressBarJs.style.background = `conic-gradient(
+  progressValueJs++;
+  valueContainerJs.textContent = `${progressValueJs}%`;
+  progressBarJs.style.background = `conic-gradient(
         #ffb400 ${progressValueJs * 3.6}deg,
         #252525 ${progressValueJs * 3.6}deg
     )`;
-    if(progressValueJs == progressEndValueJs){
-        clearInterval(progressJs);
-    }
+  if (progressValueJs == progressEndValueJs) {
+    clearInterval(progressJs);
+  }
 }, speed);
 
 // PHOTOSHOP
 let progressPs = setInterval(() => {
-    progressValuePs++;
-    valueContainerPs.textContent = `${progressValuePs}%`;
-    progressBarPs.style.background = `conic-gradient(
+  progressValuePs++;
+  valueContainerPs.textContent = `${progressValuePs}%`;
+  progressBarPs.style.background = `conic-gradient(
         #ffb400 ${progressValuePs * 3.6}deg,
         #252525 ${progressValuePs * 3.6}deg
     )`;
-    if(progressValuePs == progressEndValuePs){
-        clearInterval(progressPs);
-    }
+  if (progressValuePs == progressEndValuePs) {
+    clearInterval(progressPs);
+  }
 }, speed);
 
-
-
-
-function openMenu(){
-    menuBar.classList.add('active');
-    closeMenu.classList.add('active');
-    allSideBar.classList.add('active');
+function openMenu() {
+  menuBar.classList.add("active");
+  closeMenu.classList.add("active");
+  allSideBar.classList.add("active");
 }
 
-function menuCloes(){
-    closeMenu.classList.remove('active');
-    menuBar.classList.remove('active');
-    allSideBar.classList.remove('active');
+function menuCloes() {
+  closeMenu.classList.remove("active");
+  menuBar.classList.remove("active");
+  allSideBar.classList.remove("active");
 }
